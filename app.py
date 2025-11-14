01d45b8b834f6d2218e78aee0296c8a86fd4250d
gauravgangwar465-tech <gauravgangwar465@gmail.com>
2025-11-15 01:28:16 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"message": "Finance Companion Backend Running"}
+
+@app.route("/add-expense", methods=["POST"])
+def add_expense():
+    data = request.json
+    return {"status": "success", "data": data}
+
+if __name__ == "__main__":
+    app.run()
